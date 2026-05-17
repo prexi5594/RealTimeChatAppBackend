ee72a2c7d0cad66ee9ab21908f793d0fd230d1c2
kibetkoech <vickibelangs@gmail.com>
2026-05-17 15:18:09 +0300

    added user_routes

---
+++ routes/user_routes.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, jsonify
+
+from models.user_model import User
+
+# Create Blueprint
+user_bp = Blueprint("users", __name__)
+
+
+# GET ALL USERS
+@user_bp.route("/users", methods=["GET"])
+def get_users():
+
+    users = User.query.all()
+
+    user_list = []
+
+    for user in users:
+
+        user_list.append({
+            "id": user.id,
+            "username": user.username
+        })
+
+    return jsonify(user_list), 200
+
+
+# GET SINGLE USER
+@user_bp.route("/users/<int:id>", methods=["GET"])
+def get_user(id):
+
+    user = User.query.get(id)
+
+    if not user:
+        return jsonify({
+            "message": "User not found"
+        }), 404
+
+    return jsonify({
+        "id": user.id,
+        "username": user.username
+    }), 200
