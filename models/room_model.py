03cf75bdaeeda0374257a8375a6d7e89b4020dc4
kibetkoech <vickibelangs@gmail.com>
2026-05-18 20:35:17 +0300

    done

--- models/room_model.py
+++ models/room_model.py
@@ -1,26 +1,9 @@
 from db import db
-from datetime import datetime, timezone
 
 class Room(db.Model):
-    __tablename__ = "rooms"
-
     id = db.Column(db.Integer, primary_key=True)
     room_name = db.Column(
         db.String(100),
         unique=True,
         nullable=False
     )
-    created_by = db.Column(
-        db.Integer,
-        db.ForeignKey('users.id'),
-        nullable=True
-    )
-    created_at = db.Column(
-        db.DateTime,
-        default=lambda: datetime.now(timezone.utc)
-    )
-
-    creator = db.relationship('User', backref='rooms')
-
-    def __repr__(self):
-        return f"<Room {self.room_name}>"
