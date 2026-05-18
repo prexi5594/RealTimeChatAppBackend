af594af7463560b37e914204edc27bfafc38d732
kibetkoech <vickibelangs@gmail.com>
2026-05-18 16:21:53 +0300

    update

--- db.py
+++ db.py
@@ -4,15 +4,3 @@ db = SQLAlchemy()
 
 
 from app import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(
-        db.String(100),
-        unique=True,
-        nullable=False
-    )
-    password = db.Column(
-        db.String(300),
-        nullable=False
-    )
