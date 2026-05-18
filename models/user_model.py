4244e8e458b8fe04831e6511882d0a070ff9c696
ryaan540! <rkinyua841@gmail.com>
2026-05-18 09:43:43 +0300

    REAL-14 Creating the model

---
+++ models/user_model.py
@@ -0,0 +1,13 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.String(300),
+        nullable=False
+    )
