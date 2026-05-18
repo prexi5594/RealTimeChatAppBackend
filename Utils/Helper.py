3e7f4995f6f3dfff39826cc6843478f711898fce
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-05-18 16:15:19 +0300

    Add Utils folder from REAL-20 branch

---
+++ Utils/Helper.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+# Validate request fields
+
+def validate_fields(data, fields):
+
+    for field in fields:
+
+        if field not in data:
+            return False
+
+        if not data[field]:
+            return False
+
+    return True
+
+
+def format_timestamp(timestamp):
+    formatted_time = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    
+    return formatted_time
