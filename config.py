b4aec3c3948e7584dd2fc3670fd6e81af4bb2419
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-05-20 00:59:36 +0300

    fix local env setup

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    db_url = os.environ.get("DATABASE_URL")
-
-    SQLALCHEMY_DATABASE_URI = (
-        db_url.replace("postgres://", "postgresql+psycopg2://")
-        if db_url else None
-    )
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
