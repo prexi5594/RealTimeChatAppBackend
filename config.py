7b760395a4ef0c72941baa8cd5edeae13917e569
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-05-20 00:44:08 +0300

    fix postgres driver to psycopg2 stable

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ class Config:
     db_url = os.environ.get("DATABASE_URL")
 
     SQLALCHEMY_DATABASE_URI = (
-        db_url.replace("postgres://", "postgresql+psycopg://")
+        db_url.replace("postgres://", "postgresql+psycopg2://")
         if db_url else None
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
