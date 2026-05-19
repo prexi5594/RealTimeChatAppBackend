80e867c2ac4070eb269155b8eda51a9eccb4ecc5
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-05-20 00:08:58 +0300

    fix sqlite to postgres migration

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database/chat.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
