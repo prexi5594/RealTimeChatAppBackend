29254a5bc7215426b8e743c2169ee5b1067e44f7
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-05-20 00:38:55 +0300

    force psycopg3 driver instead of psycopg2

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace(
-        "postgres://", "postgresql+psycopg://"
+    db_url = os.environ.get("DATABASE_URL")
+
+    SQLALCHEMY_DATABASE_URI = (
+        db_url.replace("postgres://", "postgresql+psycopg://")
+        if db_url else None
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
