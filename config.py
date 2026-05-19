b858451842ad4d9e26d0bd4b91df60cfc73d87e5
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-05-20 00:35:12 +0300

    fix postgres driver compatibility

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace(
+        "postgres://", "postgresql+psycopg://"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
