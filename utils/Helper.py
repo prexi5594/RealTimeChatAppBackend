21fe2a6f76dc2bef03aba7fbf9d3a2fea0165d55
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-05-29 20:56:28 +0300

    .

--- utils/Helper.py
+++ utils/Helper.py
@@ -1,70 +1,66 @@
 from flask_mail import Message
 from threading import Thread
 
+
 def validate_fields(data, fields):
 
+    for field in fields:
 
- for field in fields:
+        if field not in data:
+            return False
 
-    if field not in data:
-        return False
+        if not data[field]:
+            return False
 
-    if not data[field]:
-        return False
-
- return True
+    return True
 
 
 def format_timestamp(timestamp):
 
+    formatted_time = timestamp.strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
 
- formatted_time = timestamp.strftime(
-    '%Y-%m-%d %H:%M:%S'
-)
-
- return formatted_time
+    return formatted_time
 
 
 def send_async_email(app, mail, msg):
 
+    with app.app_context():
 
- with app.app_context():
+        try:
 
-    try:
+            mail.send(msg)
 
-        mail.send(msg)
+            print("EMAIL SENT SUCCESSFULLY")
 
-        print("EMAIL SENT SUCCESSFULLY")
+        except Exception as e:
 
-    except Exception as e:
-
-        print("OTP EMAIL ERROR:", str(e))
+            print("OTP EMAIL ERROR:", str(e))
 
 
 def send_otp_email(app, mail, email, otp):
 
+    msg = Message(
+        subject="QuickChat OTP Code",
 
- msg = Message(
-    subject="QuickChat OTP Code",
+        sender=app.config.get(
+            "MAIL_USERNAME"
+        ),
 
-    sender=app.config.get(
-        "MAIL_USERNAME"
-    ),
-
-    recipients=[email]
-)
-
- msg.body = (
-    f"Your OTP code is: {otp}"
-)
-
-Thread(
-    target=send_async_email,
-
-    args=(
-        app,
-        mail,
-        msg
+        recipients=[email]
     )
- ).start()
 
+    msg.body = (
+        f"Your OTP code is: {otp}"
+    )
+
+    Thread(
+        target=send_async_email,
+
+        args=(
+            app,
+            mail,
+            msg
+        )
+    ).start()
